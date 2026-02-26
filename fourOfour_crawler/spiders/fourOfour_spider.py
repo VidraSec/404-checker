d42cbfb198b009498a6bbce856d4d5541ee58a1e
Martin Grottenthaler <9715799+mgrottenthaler@users.noreply.github.com>
2026-02-26 16:41:26 +0100

    add ignored domains

--- fourOfour_crawler/spiders/fourOfour_spider.py
+++ fourOfour_crawler/spiders/fourOfour_spider.py
@@ -16,7 +16,7 @@ class FourOfourSpider(CrawlSpider):
     start_urls = ['http://localhost:1313/']
 
     # to ignore domains that respond with wrong error codes
-    reply_code_wrong = ()
+    reply_code_wrong = ("linkedin.com", "udemy.com", "medium.com", "research.ifcr.dk")
 
     rules = (
         # follow links internally
