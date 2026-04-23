da52260257e9daa640b1b35b8b83ae2707defafe
Martin Grottenthaler <9715799+mgrottenthaler@users.noreply.github.com>
2026-04-23 09:03:51 +0200

    add pingcastle

--- fourOfour_crawler/spiders/fourOfour_spider.py
+++ fourOfour_crawler/spiders/fourOfour_spider.py
@@ -16,7 +16,7 @@ class FourOfourSpider(CrawlSpider):
     start_urls = ['http://localhost:1313/']
 
     # to ignore domains that respond with wrong error codes
-    reply_code_wrong = ("linkedin.com", "udemy.com", "medium.com", "research.ifcr.dk", "shodan.io", "praetorian.com")
+    reply_code_wrong = ("linkedin.com", "udemy.com", "medium.com", "research.ifcr.dk", "shodan.io", "praetorian.com", "pingcastle.com")
 
     def start_requests(self):
         # Start from homepage (discovers linked pages)
